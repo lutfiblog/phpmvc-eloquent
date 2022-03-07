dcc4616795b06c2a2574edf30da48781b19be0fc
Lutfi Ikbal Majid <lutfiikbalmajid2@gmail.com>
2022-03-07 08:33:49 +0700

    add first

---
+++ app/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+class UserController extends Controller {
+ public function index()
+ {
+  $user = User::Create([    'name' => "Ahmed Khan",    'email' => "ahmed.khan@lbs.com",    'password' => password_hash("ahmedkhan",PASSWORD_BCRYPT), ]);
+print_r($user->todo()->create([
+   'todo' => "Working with Eloquent Without PHP",
+   'category' => "eloquent",
+   'description' => "Testing the work using eloquent without laravel"
+   ]));
+}
+}
---
+++ app/models/Todo.php
@@ -0,0 +1,6 @@
+<?php
+use Illuminate\Database\Eloquent\Model as Eloquent;
+class Todo extends Eloquent
+{
+   protected $fillable = ['todo','category','description'];
+}
